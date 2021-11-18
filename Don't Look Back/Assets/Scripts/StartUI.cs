db0ae478d6c645f5603080fb3fbeeefb9e212d90
1762968907 <1762968907@qq.com>
2021-11-18 09:46:10 -0500

    Video Panel/Rain/Hide Path

--- Don't Look Back/Assets/Scripts/StartUI.cs
+++ Don't Look Back/Assets/Scripts/StartUI.cs
@@ -1,31 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class StartUI : MonoBehaviour
 {
+    [SerializeField]
+    GameObject StartMenu;
+
+    [SerializeField]
+    GameObject HelpMenu;
+
+    [SerializeField]
+    VideoPlayer HelperVideoPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        HelpMenu.SetActive(false);
+        Helper();
     }
 
     public void StartGame() {
         SceneManager.LoadScene("Game");
     }
 
-    public void Helper() { 
-        
+    public void Helper() {
+        StartMenu.SetActive(false);
+        HelpMenu.SetActive(true);
+        HelperVideoPlayer.Play();
     }
 
     public void QuitGame() {
         Application.Quit();
+    }
+
+    public void ReturnToStart()
+    {
+        StartMenu.SetActive(true);
+        HelpMenu.SetActive(false);
+        HelperVideoPlayer.Stop();
     }
 }
